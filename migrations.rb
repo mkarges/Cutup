da736553e03c1d4fbdee3b5a28e171789acd1803
Matthew Karges <karges@gmail.com>
2010-10-30 13:40:29 -0700

    deleted old files

--- migrations.rb
+++
@@ -1,5 +0,0 @@
-DB.create_table :temp do
-  primary_key :id
-  String :entry
-end
-
--- p_array.rb
+++
@@ -1,6 +0,0 @@
-class Array
-  def shuffle!
-    size.downto(1) { |n| push delete_at(rand(n)) }
-    self
-  end
-end
